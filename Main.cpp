9fd828f643f03e7202a3a94cde600fafcecbe0f7
cefadime <162296721+cefadime@users.noreply.github.com>
2024-12-15 21:37:58 +0300

    random yem güncellemesi

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,6 @@
 #include "icb_gui.h"
+#include <cstdlib> // random sayı için gerekli
+#include <ctime>   //random sayıların değişkenliği için
 
 ICBYTES gameBoard, panel;
 int FRM1, BTN;
@@ -18,20 +20,22 @@ int grid[10][10] = {
     {0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
 };
 
-int food[10][10];
+bool food[10][10];
+
 void InitializeFood() {
+    srand(static_cast<unsigned>(time(0))); //yemlerin rastgele yerlerde bulunması için klendi
+
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            if (grid[i][j] == 0) {
-                food[i][j] = 1; // tum bosluklara yem koyuldu
+            if (grid[i][j] == 0) { // Sadece boşluklara yem yerleştir
+                food[i][j] = (rand() % 2 == 0); // Yem var mı yok mu rastgele belirle %50 olasılık var
             }
             else {
-                food[i][j] = 0; 
+                food[i][j] = false;
             }
         }
     }
 }
-
 
 void ICGUI_Create() {
     ICG_MWTitle("PacmanAI");
@@ -39,7 +43,6 @@ void ICGUI_Create() {
     ICG_SetFont(30, 12, "");
 }
 
-// �izilen k�s�m
 void DrawGame() {
     CreateImage(gameBoard, 300, 300, ICB_UINT);
     FillRect(gameBoard, 0, 0, 300, 300, 0x000000);
@@ -49,30 +52,31 @@ void DrawGame() {
             int x = j * 30;
             int y = i * 30;
             if (grid[i][j] == 1) {
-                FillRect(gameBoard, x, y, 30, 30, 0x8B4513);
+                FillRect(gameBoard, x, y, 30, 30, 0x8B4513); // Duvarlar
             }
         }
     }
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            if (food[i][j] == 1) {
+            if (food[i][j]) {
                 int x = j * 30 + 15;
                 int y = i * 30 + 15;
-                FillCircle(gameBoard, x, y, 3, 0x00ff00); //yemler
+                FillCircle(gameBoard, x, y, 3, 0x00ff00); // Yemlerin çizimi
             }
         }
     }
 
-    // Pacmanin cizimi 
+    //pacmanin çizimi
     FillCircle(gameBoard, pacmanX * 30 + 15, pacmanY * 30 + 15, 9, 0xffff00);
-    FillCircle(gameBoard, ghostX * 30 + 15, ghostY * 30 + 15, 7, 0xff0000); //hayalet
 
+    //hayaletin çizimi
+    FillCircle(gameBoard, ghostX * 30 + 15, ghostY * 30 + 15, 7, 0xff0000);
 
     DisplayImage(FRM1, gameBoard);
 }
-void GhostMove() {
 
-    //ai ile hareket edecek
+void GhostMove() {
+    // AI ile hayaletin hareketi buraya gelecek
 }
 
 void PacmanMove(void* lpParam) {
@@ -86,18 +90,20 @@ void PacmanMove(void* lpParam) {
         else if (key == 38) nextY--;
         else if (key == 40) nextY++;
 
-        if (nextX >= 0 && nextX < 10 && nextY >= 0 && nextY < 10 && grid[nextY][nextX] == 0) { //sinirlari ve duvari kontrol eder
+        if (nextX >= 0 && nextX < 10 && nextY >= 0 && nextY < 10 && grid[nextY][nextX] == 0) {
             pacmanX = nextX;
             pacmanY = nextY;
         }
-        if (food[pacmanY][pacmanX] == 1) {
-            food[pacmanY][pacmanX] = 0; // Yem yenilmiştir
+        if (food[pacmanY][pacmanX]) {
+            food[pacmanY][pacmanX] = false; // Yem yenildiğinde false yapılır
         }
+
         GhostMove();
         DrawGame();
+
         if (pacmanX == ghostX && pacmanY == ghostY) {
-            //oyun bitti mesajı 
-            exit(0); 
+            // Oyun bitti mesajı ekle bitir
+            exit(0);
         }
 
         Sleep(200);
